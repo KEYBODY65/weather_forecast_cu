1d478181ffbc446d8a280a861e80e4e911ca6cf7
keybody65 <popov27051971@gmail.com>
2024-12-08 14:28:15 +0300

    Task1

--- back/main.py
+++ back/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, json
-from req import req, get_weather_data
+from req import req, get_weather_data, get_loc_code_by_coords
 
 
 app = Flask(__name__)
@@ -7,7 +7,9 @@ app.config["SECRET_KEY"] = "yfdwk64qowyerhr124429478293"
 
 @app.route("/")
 def main():
-    return json.dumps(get_weather_data(), indent=4)
+    g = req('Москва')
+    k = get_loc_code_by_coords(g)[0]
+    return json.dumps(get_weather_data(k), indent=4)
 
 
 
--- back/req.py
+++ back/req.py
@@ -1,23 +1,41 @@
 import requests
-api_key = '0RvWYG0TzMAt1XOemk0dwSBWGyAjKVjs'
-city = 'Москва'
-location_url = f'http://dataservice.accuweather.com/locations/v1/cities/search?apikey={api_key}&q={city}&language=ru'
+API_KEY = 'd9E3GspKBOCQyA5I9MIpBldoilHzbfwy'
 
 
-def req():
+
+def req(city):
+    location_url = f'http://dataservice.accuweather.com/locations/v1/cities/search?apikey={API_KEY}&q={city}&language=ru'
     try:
         response = requests.get(location_url)
         data = response.json()
         if data:
-            return data[0]['Key']
+            lat = data[0]['GeoPosition']['Latitude']
+            long = data[0]['GeoPosition']['Longitude']
+            return [lat, long]
     except requests.exceptions.RequestException as e:
         print(f'Ошибка запроса: {e}')
         return None
 
-def get_weather_data():
+def get_loc_code_by_coords(city):
+    city_cor = f'{city[0]},{city[1]}'
+    location_url = f'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey={API_KEY}&q={city_cor}'
+    try:
+        response = requests.get(location_url)
+        data = response.json()
+        if data:
+            return [data['Key'], data['AdministrativeArea']['LocalizedName']]
+    except requests.exceptions.RequestException as e:
+        print(f'Ошибка запроса: {e}')
+        return None
+
+
+
+
+    
+
+def get_weather_data(loc_key):
     '''Фукция получения данных о погоде'''
-    k = req() # получения ключа города
-    weather_url = f'http://dataservice.accuweather.com/currentconditions/v1/{k}?apikey={api_key}&details=true' # запрос к api
+    weather_url = f'http://dataservice.accuweather.com/currentconditions/v1/{loc_key}?apikey={API_KEY}&details=true' # запрос к api
     try:
         weather_r = requests.get(weather_url) # гет-запрос к api
         weather_data = weather_r.json()[0]
